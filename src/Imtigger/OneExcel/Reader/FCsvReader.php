6880b0f540ded6900671ccae13398e6c3e7ab976
Tiger Fok <tiger@tiger-workshop.com>
2017-08-11 00:20:04 +0800

    fgetcsv remove BOM

--- src/Imtigger/OneExcel/Reader/FCsvReader.php
+++ src/Imtigger/OneExcel/Reader/FCsvReader.php
@@ -9,12 +9,18 @@ class FCsvReader extends OneExcelReader implements OneExcelReaderInterface
     public static $input_format_supported = [Format::CSV];
     private $handle;
 
-    public function load($filename, $output_format = Format::CSV, $input_format = Format::CSV)
+    public function load($filename, $input_format = Format::CSV)
     {
         $this->checkFormatSupported($input_format);
         $this->input_format = $input_format;
 
         $this->handle = fopen($filename, "r");
+
+        // Consume potential BOM, if not BOM rewind file
+        $bom = fread($this->handle, 3);
+        if (bin2hex($bom) !== 'efbbbf') {
+            rewind($this->handle);
+        }
 
         if ($this->handle === FALSE) {
             throw new \Exception("{$filename} cannot be opened.");
