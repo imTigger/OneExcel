c550e7f335d58c512ca08b78e4db424bacfab61f
Tiger Fok <tiger@tiger-workshop.com>
2017-08-11 00:22:45 +0800

    Added Reader Test

---
+++ tests/FCsvReaderTest.php
@@ -0,0 +1,43 @@
+<?php
+
+use Imtigger\OneExcel\Driver;
+use PHPUnit\Framework\TestCase;
+
+final class FCsvReaderTest extends TestCase {
+    public function testReader()
+    {
+        $excel = \Imtigger\OneExcel\OneExcelReaderFactory::create()->fromFile(__DIR__ . '/01simple.csv')->withDriver(Driver::FGETCSV)->make();
+        $this->assertInstanceOf(\Imtigger\OneExcel\Reader\FCsvReader::class, $excel);
+
+        $i = 1;
+        foreach ($excel->row() as $row) {
+            if ($i == 1) {
+                $this->assertEquals("Hello", $row[0]);
+                $this->assertEmpty( $row[1]);
+                $this->assertEquals("Hello", $row[2]);
+                $this->assertEmpty($row[3]);
+            } else if ($i == 2) {
+                $this->assertEmpty($row[0]);
+                $this->assertEquals("world!", $row[1]);
+                $this->assertEmpty( $row[2]);
+                $this->assertEquals("world!", $row[3]);
+            } else if ($i == 3) {
+                $this->assertEmpty($row[0]);
+                $this->assertEmpty($row[1]);
+                $this->assertEmpty($row[2]);
+                $this->assertEmpty($row[3]);
+            } else if ($i == 4) {
+                $this->assertEquals('Miscellaneous glyphs', $row[0]);
+                $this->assertEmpty($row[1]);
+                $this->assertEmpty($row[2]);
+                $this->assertEmpty($row[3]);
+            } else if ($i == 5) {
+                $this->assertEquals('éàèùâêîôûëïüÿäöüç', $row[0]);
+                $this->assertEmpty($row[1]);
+                $this->assertEmpty($row[2]);
+                $this->assertEmpty($row[3]);
+            }
+            $i += 1;
+        }
+    }
+}
