25952a7337945910199a95f6b5e0d0320a092f48
Tiger Fok <tiger@tiger-workshop.com>
2018-09-15 22:26:34 +0800

    Added examples

---
+++ examples/load-from-template-csv-to-xlsx.php
@@ -0,0 +1,19 @@
+<?php
+require __DIR__ . '/../vendor/autoload.php';
+
+use Imtigger\OneExcel\OneExcelWriterFactory;
+use Imtigger\OneExcel\ColumnType;
+
+$template = __DIR__ . '/../templates/template.csv';
+$output = basename(__FILE__ . '.xlsx');
+$excel = OneExcelWriterFactory::create()->fromFile($template)->toFile($output)->make();
+
+$excel->writeCell(2, 0, 'Hello');
+$excel->writeCell(3, 1, 'World');
+$excel->writeCell(4, 2, 3.141592653, ColumnType::NUMERIC);
+$excel->writeRow(5, ['One', 'Excel']);
+
+$excel->output();
+
+echo "File {$output} created using " . get_class($excel);
+
