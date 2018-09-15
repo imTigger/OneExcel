9b579432d8ab220cde919230007085ee87e7b04e
Tiger Fok <tiger@tiger-workshop.com>
2018-09-16 02:28:10 +0800

    Updated stream example

--- examples/stream-csv.php
+++ examples/stream-csv.php
@@ -2,22 +2,19 @@
 require __DIR__ . '/../vendor/autoload.php';
 
 use Imtigger\OneExcel\OneExcelWriterFactory;
-use Imtigger\OneExcel\ColumnType;
 use Imtigger\OneExcel\Driver;
 use Imtigger\OneExcel\Format;
 
-$template = __DIR__ . '/../templates/template.xlsx';
 $output = basename(__FILE__ . '.csv');
 $excel = OneExcelWriterFactory::create()
-    ->fromFile($template, Format::XLSX)
     ->toStream($output, Format::CSV)
     ->withDriver(Driver::SPOUT)
     ->make();
 
-$excel->writeCell(2, 0, 'Hello');
-$excel->writeCell(3, 1, 'World');
-$excel->writeCell(4, 2, 3.141592653, ColumnType::NUMERIC);
-$excel->writeRow(5, ['One', 'Excel']);
+for ($i = 1; $i < 1048576; $i += 1) {
+    usleep(10); // Artificial delay
+    $excel->writeRow($i, ['One', 'Excel', 'Test', $i]);
+}
 
 $excel->output();
 
