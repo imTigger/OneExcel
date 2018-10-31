493e5bea9ee993d92a72b41d7dbb2b3ecfe179fd
atlas-wong <atlas-wong@users.noreply.github.com>
2018-10-31 16:52:39 +0800

    Update PhpSpreadsheetWriter.php
    Added setSheet function to PhpSpreadsheetWriter.php

--- src/Imtigger/OneExcel/Writer/PhpSpreadsheetWriter.php
+++ src/Imtigger/OneExcel/Writer/PhpSpreadsheetWriter.php
@@ -36,6 +36,11 @@ class PhpSpreadsheetWriter extends OneExcelWriter implements OneExcelWriterInter
         $this->book = $objReader->load($filename);
         $this->sheet = $this->book->getActiveSheet();
     }
+    
+    public function setSheet($id)
+    {
+        $this->sheet = $this->book->setActiveSheetIndex($id);
+    }
 
     public function writeCell($row_num, $column_num, $data, $data_type = null)
     {
